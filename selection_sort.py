76d1ec221955217bc2a99dd01a11391494bd9cf3
Kushagr Gupta <kushagr.gupta@nsi.lab>
2023-07-28 12:08:27 +0530

    selection sort

---
+++ selection_sort.py
@@ -0,0 +1,25 @@
+nums = list()
+
+entries = str(input("Enter the numbers to be sorted: "))
+num = entries.split(",")
+nums = [int(i) for i in num]
+
+print(nums)
+n = len(nums)
+
+def swap(i, ind, temp):
+    nums[ind] = nums[i]
+    nums[i] = temp
+
+def selection_sort(nums, n):
+    for i in range(0, n-1):
+        temp = nums[i]
+        for j in range(i, n-1):
+            if nums[j+1] < temp:
+                temp = nums[j+1]
+                ind = j+1
+        if temp != nums[i]:
+            swap(i, ind, temp)
+
+selection_sort(nums, n)
+print(nums)
